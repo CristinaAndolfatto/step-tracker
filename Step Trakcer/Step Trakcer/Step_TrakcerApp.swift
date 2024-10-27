a7b5d7213052f1e44f1b8a041d1fc7ebc820651e
Cristina Andolfatto <andolfattocristina@gmail.com>
2024-10-27 15:44:47 +0100

    feat: created XCode project

---
+++ Step Trakcer/Step Trakcer/Step_TrakcerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrakcerApp.swift
+//  Step Trakcer
+//
+//  Created by Cristina Andolfatto on 27/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrakcerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

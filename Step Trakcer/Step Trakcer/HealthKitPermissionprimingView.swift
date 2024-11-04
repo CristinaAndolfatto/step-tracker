9c58c581bcfc23e724937cbeee2a2c4c5b949d0d
Cristina Andolfatto <andolfattocristina@gmail.com>
2024-11-04 23:10:27 +0100

    feat: first ui setup for healthkit permission

---
+++ Step Trakcer/Step Trakcer/HealthKitPermissionprimingView.swift
@@ -0,0 +1,44 @@
+//
+//  HealthKitPermissionprimingView.swift
+//  Step Trakcer
+//
+//  Created by Cristina Andolfatto on 04/11/24.
+//
+
+import SwiftUI
+
+struct HealthKitPermissionprimingView: View {
+    
+    let description =
+    """
+This app displays your step and weight data in interactive charts.
+    
+You can also add new data to Apple Health from this app. Your data is private and secure
+"""
+    
+    var body: some View {
+        VStack(spacing: 130) {
+            VStack(alignment: .leading) {
+                Image(ImageResource.iconAppleHealth)
+                    .resizable()
+                    .frame(width: 90, height: 90)
+                    .shadow(color: .gray.opacity(0.3), radius: 16)
+                    .padding(.bottom, 12)
+                Text("Apple Health Integration")
+                    .font(.title2.bold())
+                Text(description)
+                    .foregroundStyle(.secondary)
+            }
+            Button("Connect Apple Health") {
+                // TODO: connect to apple health
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.pink)
+        }
+        .padding(30)
+    }
+}
+
+#Preview {
+    HealthKitPermissionprimingView()
+}
